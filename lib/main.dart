965fedf6ee01ad003e103ce7a69360485441b6b3
Bilawal Mehmood <jattbilawaljatt786@gmail.com>
2022-05-29 15:09:03 +0500

    optimized code

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:todoapp/Screens/Starter/splash_screen.dart';
+import 'package:todoapp/screens/starter/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
